e1f379e739cd20119079ed0831146f116ea438dc
jakehayes12798 <jakehayes12798@gmail.com>
2025-05-23 14:04:51 -0400

    v1.9.2 CalliAPI to GitHub Pages with index.html

--- obj/Release/net8.0-windows/CalliAPI.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/CalliAPI.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalliAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.9.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.9.1+c94d452e72be371de9e6b96a86335defc2634f23")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.9.2.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.9.2+c765acdb66286c67dceb853e9a776358357d74b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalliAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalliAPI")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.9.1.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.9.2.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
