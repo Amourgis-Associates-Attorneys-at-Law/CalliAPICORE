9a1696dfb1815a908b2230e75043c2e07c734086
jakehayes12798 <jakehayes12798@gmail.com>
2025-05-23 10:27:07 -0400

    v1.7.7 add nojekyll
    Jekyll no Jekylling

--- obj/Release/net8.0-windows/CalliAPI.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/CalliAPI.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalliAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.7.6.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.7.6+758bec4890d1382e5d2e11b0553fd76ca2b98991")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.7.7.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.7.7+27842011a3f58d40096222ec8569830b3ccb358c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalliAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalliAPI")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.7.6.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.7.7.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
